80baecd0fe571399ac67f2f84bc6d9b49994bc8d
xunw <xunw16@gmail.com>
2014-07-29 16:28:03 +0800

    -a

--- src/recsys/MovLens.scala
+++ src/recsys/MovLens.scala
@@ -5,13 +5,39 @@ import org.apache.spark.SparkConf
 import scala.Tuple2
 import scala.List
 import java.util._
+import org.apache.spark.mllib.recommendation.{ALS,Rating,MatrixFactorizationModel}
 object MovLens{
   def main(args:Array[String]){
-    println("hello:")
     val sc = new SparkContext("local[4]", "movlens",System.getenv("SPARK_HOME"))
-    val arr = sc.parallelize(List(1,2,3,4), 4)
+   // val data = sc.textFile("test.data") //formatted as: user product rating
+    val data = sc.textFile("/home/xunw/data/MovieLens/ml-1m/ratings.dat")
     
-    println(arr.sum())
+    val ratings = data.map(_.split("::") match { case Array(user, item, rate,time) =>
+    Rating(user.toInt, item.toInt, rate.toDouble)
+  })
+  
+    val rank = 10  //latent factors
+    val numIterations = 20
+    val alpha = 0.01
+    
+    val model:MatrixFactorizationModel= ALS.train(ratings, rank, numIterations, alpha)
+    //val model:MatrixFactorizationModel = ALS.trainImplicit(ratings, rank, numIterations)
+  //  val model2 = ALS.
+    val userProducts = ratings.map{case Rating(user,product,rate) => (user,product)}
+    val predictions = model.predict(userProducts).map{case Rating(user,product,rate) => ((user,product),rate)}
+    val ratesAndPreds= predictions.join(ratings.map{
+      case Rating(user,product,rate) => ((user,product),rate)}
+    )
+    
+    val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) => 
+    val err = (r1 - r2)
+     err * err
+     }.mean()
+     
+    println("Mean Squared Error = " + MSE)
+    
+   
+    println(ratings.count())
   }
 
 }
